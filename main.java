18124e29c811019c720cbf133d35cdfe56eb7680
ErenSahin1 <149691633+ErenSahin1@users.noreply.github.com>
2023-11-05 18:12:59 +0300

    Update and rename to main.java

--- main.java
+++ main.java
@@ -6,11 +6,10 @@ Public class Main
 system.out.println("Bir Sayı Giriniz :") ;
 int input=sc.nextInt ();
 
-İf(input>0) {
+If(input>0) {
       System.out.println("Pozitif bir sayıdır.") ;
 {else if (input<0) {
       system.out.println("Negatif bir sayıdır") ;
 {
 
-Eren Şahin 
-Bilgisayar Teknolojisi - 2311290007
+
